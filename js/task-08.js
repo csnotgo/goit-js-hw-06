c171ca3c4c9f5122a3083f26f6531d0ef5fb74d4
Ksenia Skarzhynets <109911970+witkcher@users.noreply.github.com>
2022-10-01 14:51:42 +0300

    add task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
+const formBtn = document.querySelector("button");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const email = event.currentTarget.elements.email.value;
+  const password = event.currentTarget.elements.password.value;
+  if (email === "" || password === "") {
+    alert("Усі поля мають бути заповнені!");
+  } else {
+    const formData = {
+      email,
+      password,
+    };
+    console.log(formData);
+    form.reset();
+  }
+}
