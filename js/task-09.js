4bbe5329cf8aec4185b7131983491655e3b07f99
Ksenia Skarzhynets <109911970+witkcher@users.noreply.github.com>
2022-10-01 15:31:42 +0300

    add task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btn = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const text = document.querySelector("span");
+
+btn.addEventListener("click", (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  text.textContent = getRandomHexColor();
+});
