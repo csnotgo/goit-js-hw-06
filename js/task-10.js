f852dc3f4d4ac595e63a0bb152d841cfb43dd95c
Ksenia Skarzhynets <109911970+witkcher@users.noreply.github.com>
2022-10-02 12:09:52 +0300

    .

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+// const boxes = document.querySelector("#boxes");
+// const createBtn = document.querySelector("[data-create]");
+// const destroyBtn = document.querySelector("[data-destroy]");
+// const input = document.querySelector("input");
+
+// function createBoxes(amount) {
+//   const box = document.createElement("div");
+//   box.style.width = 30 + "px";
+//   box.style.height = 30 + "px";
+//   box.style.backgroundColor = getRandomHexColor();
+//   boxes.insertAdjacentElement("afterbegin", box);
+// }
+
+// input.addEventListener("change", (event) => {
+//   event.currentTarget.value;
+// });
+
+// createBtn.addEventListener("click", createBoxes);
